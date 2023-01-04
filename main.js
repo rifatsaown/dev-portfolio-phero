ca5bcdae519264a0926853b7f68c6277099d0836
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-01-05 01:46:03 +0600

    btn created

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 function btnClick() {
     console.log("Button clicked");
-    document.querySelector(".btn-primary").innerHTML = "Noob Dev Can't be Hired";
+    document.querySelector(".hire-btn").innerHTML = "Noob Dev Can't be Hired";
 }

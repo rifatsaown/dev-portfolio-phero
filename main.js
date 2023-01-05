0f420c54533a750f9acaa87f4ba05ff1f80c1699
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-01-06 00:45:05 +0600

    banner section motamoti hoicher

--- main.js
+++ main.js
@@ -2,3 +2,6 @@ function btnClick() {
     console.log("Button clicked");
     document.querySelector(".hire-btn").innerHTML = "Noob Dev Can't be Hired";
 }
+const makeCall = () => {
+    window.open('tel:+8801626199913');
+}

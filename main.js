24c24aebd5d775e8a839a79a535f122f6201f5fb
Md Rifat Hossen <rifatsaown0@gmail.com>
2023-01-11 01:47:19 +0600

    other swection added

--- main.js
+++ main.js
@@ -5,3 +5,9 @@ function btnClick() {
 const makeCall = () => {
     window.open('tel:+8801626199913');
 }
+const sendMail = () => {
+    window.open('mailto:"rifatsaown0@gmail.com"');
+}
+const openMap = () => {
+    window.open('https://goo.gl/maps/6XJFjK7V2QH2');
+}
